2204897cbd6021b138c7b01da1d62e4978c69ac0
just-samad <118440568+just-samad@users.noreply.github.com>
2025-04-19 12:12:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ function toggleTheme() {
   const icon = document.querySelector('.theme-toggle i');
   icon.classList.toggle('fa-moon');
   icon.classList.toggle('fa-sun');
+  const menuToggle = document.getElementById('menu-toggle');
+  const navLinks = document.getElementById('nav-links');
+
 }
 
 function closePopup() {
   document.getElementById('popup').classList.add('hidden');
 }
 
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
 window.onload = function () {
   setTimeout(() => {
     document.getElementById('popup').classList.remove('hidden');
   }, 1000);
+
 };
